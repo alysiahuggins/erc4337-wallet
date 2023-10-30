432023fee6dfe976f9547a08f96f0c1da3e5b4cf
Alysia Huggins <hi@alysia.tech>
2023-10-30 21:31:05 +0700

    added tests for BatchedWalletFactory

--- contracts/test/BatchedWallet.t.sol
+++ contracts/test/BatchedWallet.t.sol
@@ -3,13 +3,16 @@ pragma solidity >=0.8.12 <=0.8.20;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {BatchedWallet} from "../src/BatchedWallet.sol";
+import {IEntryPoint} from "account-abstraction/interfaces/IEntryPoint.sol";
+
 
 
 contract BatchedWalletTest is Test {
     BatchedWallet public bw;
+    IEntryPoint entryPoint = IEntryPoint(address(10101));
 
     function setUp() public {
-        // bw = new BatchedWallet();
+        bw = new BatchedWallet(entryPoint);
     }
     
 }
